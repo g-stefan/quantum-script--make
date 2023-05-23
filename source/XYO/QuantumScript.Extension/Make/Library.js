49e7847f25ed4426567e0ff642c0b528c2577ba4
Grigore Stefan <g_stefan@yahoo.com>
2023-05-23 10:39:47 +0300

    Update

--- source/XYO/QuantumScript.Extension/Make/Library.js
+++ source/XYO/QuantumScript.Extension/Make/Library.js
@@ -17,21 +17,22 @@ MakeError.prototype.toString = function() {
 
 function Make() {
 	this.targets_ = {};
-	this.threadsCount = 8;
+	this.threadsCount = Processor.getCount();
 
 	this.target = function(target, source, proc, this_, arg) {
-		if(!Script.isFunction(proc)) {
+		if (!Script.isFunction(proc)) {
 			throw new MakeError("Invalid function provided");
 		};
-		if(Script.isNil(arg)) {
-			arg = [target, source];
+		if (Script.isNil(arg)) {
+			arg = [ target, source ];
 		};
-		if(Script.isArray(target) || Script.isObject(target)) {
+		if (Script.isArray(target) || Script.isObject(target)) {
 			Script.forEach(target, function(key, value) {
-				this.targets_[value] = [source, proc, this_, arg, false, false, false];
+				this.targets_[value] = [ source, proc, this_, arg, false, false, false ];
 			}, this);
+			return;
 		};
-		this.targets_[target] = [source, proc, this_, arg, false, false, false];
+		this.targets_[target] = [ source, proc, this_, arg, false, false, false ];
 	};
 
 	this.build = function(target) {
@@ -43,7 +44,7 @@ function Make() {
 		var done;
 		var countX;
 
-		if(Script.isNil(target)) {
+		if (Script.isNil(target)) {
 			Script.forEach(this.targets_, function(key) {
 				this.build(key);
 			}, this);
@@ -57,14 +58,14 @@ function Make() {
 
 		this.buildTreeGenerate_(buildTree, target, 0);
 
-		for(m = buildTree.length - 1; m >= 0; --m) {
-			if(!buildTree[m]) {
+		for (m = buildTree.length - 1; m >= 0; --m) {
+			if (!buildTree[m]) {
 				continue;
 			};
 
 			targetToBuild = [];
 			Script.forEach(buildTree[m], function(key) {
-				if(!this.targets_[key][5]) {
+				if (!this.targets_[key][5]) {
 					targetToBuild[targetToBuild.length] = key;
 				};
 			}, this);
@@ -73,28 +74,28 @@ function Make() {
 			threads = [];
 			done = 0;
 
-			while(count < targetToBuild.length) {
+			while (count < targetToBuild.length) {
 				CurrentThread.sleep(1);
-				if(threads.length < this.threadsCount) {
-					if(count < targetToBuild.length) {
-						threads[threads.length] = [Thread.newThread(this.targets_[targetToBuild[count]][1], this.targets_[targetToBuild[count]][2], this.targets_[targetToBuild[count]][3]), targetToBuild[count]];
+				if (threads.length < this.threadsCount) {
+					if (count < targetToBuild.length) {
+						threads[threads.length] = [ Thread.newThread(this.targets_[targetToBuild[count]][1], this.targets_[targetToBuild[count]][2], this.targets_[targetToBuild[count]][3]), targetToBuild[count] ];
 						++count;
 						continue;
 					};
 				};
 				break;
 			};
-			while(done < targetToBuild.length) {
+			while (done < targetToBuild.length) {
 				CurrentThread.sleep(1);
 				countX = 0;
-				for(k = 0; k < threads.length; ++k) {
-					if(threads[k]) {
-						if(threads[k][0].isTerminated()) {
-							if(threads[k][0].getReturnedValue()) {
-								throw(new MakeError("Build failed."));
+				for (k = 0; k < threads.length; ++k) {
+					if (threads[k]) {
+						if (threads[k][0].isTerminated()) {
+							if (threads[k][0].getReturnedValue()) {
+								throw (new MakeError("Build failed."));
 							};
-							if(!Shell.fileExists(threads[k][1])) {
-								throw(new MakeError("Build failed, target not found: " + threads[k][1]));
+							if (!Shell.fileExists(threads[k][1])) {
+								throw (new MakeError("Build failed, target not found: " + threads[k][1]));
 							};
 							this.targets_[threads[k][1]][5] = true;
 							threads[k] = null;
@@ -104,13 +105,13 @@ function Make() {
 						countX++;
 					};
 				};
-				if(countX >= this.threadsCount) {
+				if (countX >= this.threadsCount) {
 					continue;
 				};
-				for(k = 0; k < threads.length; ++k) {
-					if(!threads[k]) {
-						if(count < targetToBuild.length) {
-							threads[k] = [Thread.newThread(this.targets_[targetToBuild[count]][1], this.targets_[targetToBuild[count]][2], this.targets_[targetToBuild[count]][3]), targetToBuild[count]];
+				for (k = 0; k < threads.length; ++k) {
+					if (!threads[k]) {
+						if (count < targetToBuild.length) {
+							threads[k] = [ Thread.newThread(this.targets_[targetToBuild[count]][1], this.targets_[targetToBuild[count]][2], this.targets_[targetToBuild[count]][3]), targetToBuild[count] ];
 							++count;
 							continue;
 						};
@@ -121,17 +122,17 @@ function Make() {
 	};
 
 	this.buildTreeGenerate_ = function(buildTree, target, level) {
-		if(Script.isNil(target)) {
+		if (Script.isNil(target)) {
 			return true;
 		};
-		if(Script.isNil(buildTree[level])) {
+		if (Script.isNil(buildTree[level])) {
 			buildTree[level] = {};
 		};
-		if(this.buildTreeGenerateTarget_(buildTree, target, level)) {
+		if (this.buildTreeGenerateTarget_(buildTree, target, level)) {
 			buildTree[level][target] = true;
 			return true;
 		};
-		if(!Shell.fileExists(target)) {
+		if (!Shell.fileExists(target)) {
 			buildTree[level][target] = true;
 			return true;
 		};
@@ -139,11 +140,11 @@ function Make() {
 	};
 
 	this.buildTreeGenerateTarget_ = function(buildTree, target, level) {
-		if(Script.isNil(this.targets_[target])) {
-			throw(new MakeError("Target not found: " + target));
+		if (Script.isNil(this.targets_[target])) {
+			throw (new MakeError("Target not found: " + target));
 		};
 
-		if(this.targets_[target][4]) {
+		if (this.targets_[target][4]) {
 			Script.forEach(this.targets_, function(key, value) {
 				this.targets_[key][6] = false;
 			}, this);
@@ -153,17 +154,17 @@ function Make() {
 
 		this.targets_[target][4] = true;
 
-		if(Script.isNil(this.targets_[target][0])) {
+		if (Script.isNil(this.targets_[target][0])) {
 			buildTree[level][target] = true;
 			return true;
 		};
 
-		if(Script.isArray(this.targets_[target][0]) || Script.isObject(this.targets_[target][0])) {
+		if (Script.isArray(this.targets_[target][0]) || Script.isObject(this.targets_[target][0])) {
 			var retV;
 			retV = false;
 
 			Script.forEach(this.targets_[target][0], function(key, value) {
-				if(this.buildTreeGenerateTargetSource_(buildTree, target, value, level)) {
+				if (this.buildTreeGenerateTargetSource_(buildTree, target, value, level)) {
 					retV = true;
 				};
 			}, this);
@@ -174,22 +175,22 @@ function Make() {
 	};
 
 	this.buildTreeGenerateTargetSource_ = function(buildTree, target, source, level) {
-		if(Script.isNil(this.targets_[target])) {
-			throw(new MakeError("Target not found: " + target));
+		if (Script.isNil(this.targets_[target])) {
+			throw (new MakeError("Target not found: " + target));
 		};
 
-		if(Script.isNil(this.targets_[source])) {
-			if(!Shell.fileExists(source)) {
-				throw(new MakeError("Source not found: " + source));
+		if (Script.isNil(this.targets_[source])) {
+			if (!Shell.fileExists(source)) {
+				throw (new MakeError("Source not found: " + source));
 			};
-			if(Shell.compareLastWriteTime(target, source) < 0) {
+			if (Shell.compareLastWriteTime(target, source) < 0) {
 				buildTree[level][target] = true;
 				return true;
 			};
 			return false;
 		};
-		if(Shell.fileExists(source)) {
-			if(Shell.compareLastWriteTime(target, source) < 0) {
+		if (Shell.fileExists(source)) {
+			if (Shell.compareLastWriteTime(target, source) < 0) {
 				buildTree[level][target] = true;
 				this.buildTreeGenerate_(buildTree, source, level + 1);
 				return true;
@@ -199,17 +200,17 @@ function Make() {
 	};
 
 	this.buildTreeBumpTarget_ = function(buildTree, target, level) {
-		if(Script.isNil(this.targets_[target])) {
+		if (Script.isNil(this.targets_[target])) {
 			return;
 		};
 
-		if(this.targets_[target][6]) {
+		if (this.targets_[target][6]) {
 			return;
 		};
 
 		this.targets_[target][6] = true;
 
-		if(Script.isArray(this.targets_[target][0]) || Script.isObject(this.targets_[target][0])) {
+		if (Script.isArray(this.targets_[target][0]) || Script.isObject(this.targets_[target][0])) {
 			Script.forEach(this.targets_[target][0], function(key, value) {
 				this.buildTreeBumpSource_(buildTree, target, value, level + 1);
 			}, this);
@@ -222,8 +223,8 @@ function Make() {
 	this.buildTreeBumpSource_ = function(buildTree, target, source, level) {
 		Script.forEach(buildTree, function(key, value) {
 			Script.forEach(value, function(keyX, valueX) {
-				if(keyX == source) {
-					if(Script.isNil(buildTree[level])) {
+				if (keyX == source) {
+					if (Script.isNil(buildTree[level])) {
 						buildTree[level] = {};
 					};
 					buildTree[level][source] = true;
@@ -240,6 +241,4 @@ function Make() {
 	this.copyFile = function(target, source) {
 		Shell.copyFile(source, target);
 	};
-
 };
-
